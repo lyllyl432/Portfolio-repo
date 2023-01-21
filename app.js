cb1fc2b4fff6d13a00b66d5761dc50171394b4e4
lyllyl432 <102397775+lyllyl432@users.noreply.github.com>
2023-01-21 06:38:35 -0800

    completely responsive
    some animation added, if link is click nav will remove

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const showIn = document.querySelectorAll(".show-in");
 console.log(sliders);
 const appearOptions = {
   threshold: 0,
-  rootMargin: "0px 0px -250px 0px"
+  rootMargin: "0px 0px -350px 0px"
 }
 const appearOnScroll = new IntersectionObserver(entries =>{
   entries.forEach(entry =>{
@@ -72,3 +72,100 @@ showIn.forEach(show =>{
   progressOnScroll.observe(show);
 })
 
+//********smooth scroll *********/
+const scrollLinks = document.querySelectorAll('.scroll-link');
+scrollLinks.forEach(function(link){
+  link.addEventListener('click', function(e){
+    e.preventDefault();
+
+  //navigate to specific
+  const id = e.currentTarget.getAttribute("href").slice(1);
+  const element = document.getElementById(id);
+  console.log(element);
+  
+  //calculate the heights
+  const navHeight = nav.getBoundingClientRect().height;
+  const navMenuHeight = navMenu.getBoundingClientRect().height;
+  const fixedNav = nav.classList.contains('active');
+  let position = element.offsetTop - navHeight;
+  
+  if(!fixedNav){
+    position = position - navHeight;
+  }
+  if(navHeight > 200){
+    position = position + navMenuHeight;
+  }
+  window.scrollTo({
+    left:0,
+    top:position,
+  });
+  navMenu.classList.remove("active");
+  });
+});
+
+//Top link
+window.addEventListener("scroll", ()=>{
+  const topLink = document.querySelector(".top-link");
+  const scrollHeight = window.pageYOffset;
+  if(scrollHeight > 500){
+    topLink.classList.add('show-links')
+}
+else{
+    topLink.classList.remove('show-links')
+}
+})
+
+//text-animation
+// function([string1, string2],target id,[color1,color2])    
+consoleText(['Im John Lyl', 'I love to code', 'Made with Love.'], 'text',['tomato','rebeccapurple','lightblue']);
+
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
+
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 120)
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
+
+    } else {
+      con.className = 'console-underscore'
+
+      visible = true;
+    }
+  }, 400)
+}
+
+//stop loader
